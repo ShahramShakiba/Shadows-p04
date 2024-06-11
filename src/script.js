b14ac5fc290df30854f986e6f21efd2d3ac2ccba
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-11 22:24:30 +0330

    Baking shadows

--- src/script.js
+++ src/script.js
@@ -6,6 +6,11 @@ const canvas = document.querySelector('canvas.webgl'); // Canvas
 const scene = new THREE.Scene(); // Scene
 const gui = new GUI(); // Debug
 
+//======================= Textures ======================
+const textureLoader = new THREE.TextureLoader();
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
+bakedShadow.colorSpace = THREE.SRGBColorSpace;
+
 //======================= Lights ========================
 //======= Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
@@ -79,7 +84,10 @@ gui.add(material, 'roughness').min(0).max(1).step(0.001);
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
 sphere.castShadow = true;
 
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(5, 5),
+  new THREE.MeshBasicMaterial({ map: bakedShadow })
+);
 plane.rotation.x = Math.PI * -0.5;
 plane.position.y = -0.5;
 plane.receiveShadow = true;
@@ -108,7 +116,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(width, height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
+renderer.shadowMap.enabled = false;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 //==================== Resize Listener ===================
